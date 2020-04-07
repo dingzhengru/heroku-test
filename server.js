ec97fe0a8911494ea1cf9b0e78a57d4ab3a910c5
dingzhengru <qws7869vdx@gmail.com>
2020-04-07 14:53:56 +0800

    init

---
+++ server.js
@@ -0,0 +1,20 @@
+import express from 'express'
+import cors from 'cors'
+
+const PORT = process.env.PORT || 3000
+
+const app = express()
+
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+app.use(express.json()) // for parsing application/json
+
+app.use(cors())
+
+app.get('*', (req, res, next) => {
+  res.send('Royal Gclub API')
+})
+
+// 加上 0.0.0.0 取得的 req.ip 就會是 ipv4 的
+app.listen(PORT, () => {
+    console.log(`listening on ${ PORT }`)
+})
